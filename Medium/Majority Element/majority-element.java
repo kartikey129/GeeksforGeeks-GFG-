1ff7953dc34bab297f8923aa0ecc6e5de83638a9
Kartikey Gupta <55404306+kartikey129@users.noreply.github.com>
2024-03-18 19:06:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        int cnt=0;
+        int ele=0;
+        for(int i=0 ; i<size; i++){
+            if(cnt==0){
+                cnt=1;
+                ele=a[i];
+            }
+            else if(ele==a[i]){
+                cnt++;
+            }
+            else
+            cnt--;
+        }
+            int cnt1=0;
+            for(int j=0 ; j<size ; j++){
+                if(a[j]==ele)cnt1++;
+            }
+            if(cnt1>(size/2)) return ele;
+            return -1;
+    }
+}
