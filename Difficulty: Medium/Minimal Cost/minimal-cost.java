2ca71da04505fdb4d808deff2b1300449b2b7aeb
Kartikey Gupta <55404306+kartikey129@users.noreply.github.com>
2024-09-16 18:18:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimal Cost/minimal-cost.java
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -1,48 +1,61 @@
 //{ Driver Code Starts
 // Initial Template for Java
-
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int N = sc.nextInt();
-            int K = sc.nextInt();
-            int[] arr = new int[N];
-            for (int i = 0; i < N; i++) {
-                arr[i] = sc.nextInt();
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int minimizeCost(int arr[], int k) {
+        // code here
+        int n = arr.length;
+        int dp[] = new int[n];
+        dp[0] = 0;
+        for(int i=1; i<n; i++){
+            int mincost = Integer.MAX_VALUE;
+            for(int j=1; j<=k; j++){
+                if(i-j>=0){
+                int jump = dp[i-j]+Math.abs(arr[i]-arr[i-j]);
+                mincost = Math.min(mincost,jump);
             }
+            }
+            dp[i] = mincost;
+        }
+        return dp[n-1];
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
             Solution obj = new Solution();
-            int res = obj.minimizeCost(arr, N, K);
+            int res = obj.minimizeCost(arr, k);
+
             System.out.println(res);
         }
     }
 }
 // } Driver Code Ends
-
-
-// User function Template for Java
-
-class Solution {
-    public int minimizeCost(int arr[], int N, int K) {
-        // code here
-        if(N==0) return 0;
-        int dp[] = new int [N];
-        dp[0] = 0;
-        for(int i=1 ; i<N ; i++){
-            int minstep = Integer.MAX_VALUE;
-        for(int j=1 ; j<=K ; j++){
-            if(i-j>=0){
-            int jump = dp[i-j] + Math.abs(arr[i] - arr[i-j]);
-            minstep = Math.min(minstep , jump);
-        }
-        }
-        dp[i] = minstep;
-    }
-    return dp[N-1];
-}
-}
