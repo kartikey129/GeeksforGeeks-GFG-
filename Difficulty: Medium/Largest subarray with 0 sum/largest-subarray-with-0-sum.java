5fe0749f9ce1c22c55995f0c6aa9943ec694e1f3
Kartikey Gupta <55404306+kartikey129@users.noreply.github.com>
2024-12-07 11:56:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
@@ -1,52 +1,51 @@
 //{ Driver Code Starts
+import java.io.*;
 import java.util.*;
 
-class MaxLenZeroSumSub
-{
-
-    // Returns length of the maximum length subarray with 0 sum
-
-    // Drive method
-    public static void main(String arg[])
-    {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T > 0)
-        {
-            int n = sc.nextInt();
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++)
-                arr[i] = sc.nextInt();
-
-            GfG g = new GfG();
-            System.out.println(g.maxLen(arr, n));
-            T--;
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            // Read input array as a string, split by space, and convert to integers
+            String[] str = br.readLine().trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            // Print the result from maxLen function
+            System.out.println(new Solution().maxLen(arr));
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
-class GfG
-{
-    int maxLen(int arr[], int n)
-    {
-        // Your code here
-        int r = 0;
-        int sum = 0;
-        int maxlen = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        while(r<n){
-            sum+=arr[r];
-            if(sum==0)
-            maxlen = r+1;
-            if(map.containsKey(sum-0))
-            maxlen  = Math.max(maxlen , r - map.get(sum-0));
-            
-            if(!map.containsKey(sum))
-            map.put(sum,r);
-            r++;
+
+class Solution {
+    int maxLen(int arr[]) {
+        // code here
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int maxi = 0;
+        int prefixSum = 0;
+        int n = arr.length;
+        for(int i=0; i<n ; i++)
+        {
+            prefixSum+=arr[i];
+            if(prefixSum==0){
+                maxi = i+1;
+            }
+            else{
+            if(!hm.containsKey(prefixSum)){
+                hm.put(prefixSum,i);
+            }
+            else{
+                maxi = Math.max(maxi,i-hm.get(prefixSum));
+            }
         }
-        return maxlen;
     }
+    return maxi;
+}
 }
