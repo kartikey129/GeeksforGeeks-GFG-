fc6432a53b2c69c01788f5d8f541c93c7f89151f
Kartikey Gupta <55404306+kartikey129@users.noreply.github.com>
2025-08-25 23:20:19 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        Node curr = head;
+        while(curr!=null){
+            if(curr.data==key) return true;
+            curr = curr.next;
+        }
+        return false;
+    }
+}
